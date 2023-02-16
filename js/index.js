05203a4ff1f8222d2cf51fc7371b97f532804452
mart95 <mar.torressi.sanchez.95@gamil.com>
2023-02-16 11:59:30 -0300

    empezando a jugar con JS

---
+++ js/index.js
@@ -0,0 +1,12 @@
+//El siguiente código es cualquier cosa
+//STRING - cadenas de texto o alfanuméricas
+let empresa = 'CoderHouse 2023'
+let nombreCompleto = 'Mar Torressi'
+ 
+//NUMBER - valor numéricos
+let numeroA = 2103
+let numeroB = 1975
+let numeroC = 2101.72
+
+//CONSTANTE - valor estático o fijo
+const curso = 'JavaScript - 39470'
